c1b3f526536a3f5450b2a1a16bef833c0cb87606
Yamin Nather <2001s.yn@gmail.com>
2021-02-17 18:43:09 +0530

    Fixed Dev Tools Window by changing createNativeStackNavigator to createStackNavigator

--- Components/CLoader/CLoader.tsx
+++ Components/CLoader/CLoader.tsx
@@ -2,49 +2,31 @@ import React, {Dispatch, SetStateAction, useEffect} from "react";
 import {View, Text} from "react-native";
 
 interface Props<T> {
-  mpromise?: Promise<T>;
-  mcLoading?: React.ReactElement;
+  mpromise: Promise<T>;
+  mloadingComponent: () => React.ReactElement;
   mbuildComponent: (val: T)=>React.ReactElement;
 }
 
 export default function CLoader<T>(props: Props<T>): React.ReactElement {
+  //#region Hooks  
   const componentMounted: React.MutableRefObject<boolean> = React.useRef<boolean>(false);
   const val: [T | undefined, Dispatch<SetStateAction<T | undefined>>] = React.useState<T | undefined>(undefined);
-    
+
   useEffect(
     () => {
-      if(props.mpromise == undefined)
-        return;
-
-      props.mpromise.then(
-        (value) => val[1](value)
-      );      
-    }, 
+      props.mpromise.then((value) => val[1](value))
+    },
     [componentMounted]
   );
+  //#endregion
 
-  const faddAnotherThen: ()=>Promise<void> = async () => {
-    await sleep(2000);
-    props.mpromise?.then((value) => console.log("CustomLog:Second then called"));
-  };
-    
-  const frender: ()=>React.ReactElement = () => {
-    const fbuildLoader: ()=>React.ReactElement = () => {
-      if(props.mcLoading == undefined)
-        return(<Text>Loading...</Text>);
-      
-      return(<>{props.mcLoading}</>);
-    };
+  if(val[0] == undefined)
+    return (
+      <View style={{width: "100%", height: "100%", justifyContent: "center", alignItems: "center"}}>
+        {props.mloadingComponent()}
+      </View>
+    );
+  else
+    return(props.mbuildComponent(val[0] as T));
 
-    if(val[0] == undefined)
-      return(<>{fbuildLoader()}</>);    
-
-    return(<>{props.mbuildComponent(val[0])}</>);
-  }
-
-  return frender();
-}
-
-function sleep(ms: number): Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, ms));
 }
