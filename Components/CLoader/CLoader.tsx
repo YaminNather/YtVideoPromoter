620517b5e12e48cb4036a6c5f6ff322eb9b54ecf
Yamin Nather <2001s.yn@gmail.com>
2021-03-05 18:00:46 +0530

    Done creating a CLoader

--- Components/CLoader/CLoader.tsx
+++ Components/CLoader/CLoader.tsx
@@ -2,31 +2,14 @@ import React, {Dispatch, SetStateAction, useEffect} from "react";
 import {View, Text} from "react-native";
 
 interface Props<T> {
-  mpromise: Promise<T>;
-  mloadingComponent: () => React.ReactElement;
-  mbuildComponent: (val: T)=>React.ReactElement;
+  misLoading: boolean;
+  mloadingComponent: ()=>React.ReactElement;
+  mbuildComponent: ()=>React.ReactElement;
 }
 
 export default function CLoader<T>(props: Props<T>): React.ReactElement {
-  //#region Hooks  
-  const componentMounted: React.MutableRefObject<boolean> = React.useRef<boolean>(false);
-  const val: [T | undefined, Dispatch<SetStateAction<T | undefined>>] = React.useState<T | undefined>(undefined);
+  if(props.misLoading == true)
+    return props.mloadingComponent();  
 
-  useEffect(
-    () => {
-      props.mpromise.then((value) => val[1](value))
-    },
-    [componentMounted]
-  );
-  //#endregion
-
-  if(val[0] == undefined)
-    return (
-      <View style={{width: "100%", height: "100%", justifyContent: "center", alignItems: "center"}}>
-        {props.mloadingComponent()}
-      </View>
-    );
-  else
-    return(props.mbuildComponent(val[0] as T));
-
+  return props.mbuildComponent();
 }
