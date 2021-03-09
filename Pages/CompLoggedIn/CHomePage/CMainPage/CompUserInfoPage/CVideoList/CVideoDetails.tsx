caed24fe50c6f787360c932fd2f5c705f2fa821a
Yamin Nather <2001s.yn@gmail.com>
2021-03-09 16:16:27 +0530

    Animatng delete functionality in UserInfoPage

--- Pages/CompLoggedIn/CHomePage/CMainPage/CompUserInfoPage/CVideoList/CVideoDetails.tsx
+++ Pages/CompLoggedIn/CHomePage/CMainPage/CompUserInfoPage/CVideoList/CVideoDetails.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import {View, Image, Animated} from "react-native";
-import {Divider, IconButton} from "react-native-paper";
+import {Card, Divider, IconButton, Colors} from "react-native-paper";
 import VideoData from "../../../../../../Models/VideoData";
 import {ContextData, gmcontext as UserInfoContext, State} from "../UserInfoPageData";
 
@@ -15,27 +15,35 @@ const CVideoDetails: React.FC<Props> = (props) => {
 
   useEffect(
     () => {
-      Animated.timing(animVal, {toValue: 1, duration: 5000, useNativeDriver: true}).start();
+      const toValue: number = (contextData.mstate.minDeleteState) ? -50 : 0;
+      Animated.timing(animVal, {toValue: toValue, duration: 200, useNativeDriver: true}).start(); 
     }, 
-    [animVal]
+    [contextData.mstate.minDeleteState]
   );
   
   const frender: ()=>React.ReactElement = () => {
-    const inDeleteState: boolean = contextData.mstate.minDeleteState;
+    const inDeleteState: boolean = contextData.mstate.minDeleteState;                                    
 
     return(
       <View key={props.mkey} style={{padding: 10, flexDirection: "row", alignItems: "center"}}>  
         {/* <View style={{width: 80, height: 50, backgroundColor: "royalblue"}} /> */}
-        <Image 
-          style={{flex: 1, height: 220}}
-          source={{uri: props.mvideoData.mthumbnailURL}}
-        />
+        
+        <View style={{position: "absolute", height: "auto", right: 10, justifyContent: "center"}}>
+          {/* <View style={{borderWidth: 0.5, borderColor: Colors.black, borderRadius: 50}}> */}
+            <IconButton 
+              icon="close" onPress={() => contextData.mdeleteVideoData(props.mvideoData.mvideoId)} 
+              style={{borderWidth: 0.5, borderColor: Colors.black, borderRadius: 50}}
+            />
+          {/* </View> */}
+        </View>
 
-        {(!inDeleteState) ?
-          undefined : 
-          <IconButton icon="close" onPress={() => contextData.mdeleteVideoData(props.mvideoData.mvideoId)} />
-        }
-        <Divider />
+        <Animated.View style={{width: "100%", height: 220, transform: [ {translateX: animVal} ]}}>
+          <Card elevation={20} style={{width: "100%", height: "100%", padding: 5}}>
+            {/* <Card.Content> */}
+              <Image style={{width: "100%", height: "100%"}} source={{uri: props.mvideoData.mthumbnailURL}} />
+            {/* </Card.Content> */}
+          </Card>
+        </Animated.View>                    
       </View>
     );
   }
